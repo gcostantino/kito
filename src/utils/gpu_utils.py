3431e6546e3fa86a43951c39b4c0de0043505b69
Giuseppe Costantino <giuseppe.costantino95@gmail.com>
2026-01-26 15:03:34 +0100

    Update gpu_utils.py

--- src/utils/gpu_utils.py
+++ src/utils/gpu_utils.py
@@ -1,10 +1,23 @@
 import torch
 
-def assign_device(gpu_id: int) -> torch.device:
+'''def assign_device(gpu_id: int) -> torch.device:
     """Get the best available device with GPU ID."""
     if torch.cuda.is_available():
         return torch.device(f'cuda:{gpu_id}')
     elif torch.backends.mps.is_available():
         return torch.device(f'mps')
     else:
-        return torch.device('cpu')
+        return torch.device('cpu')'''
+
+def assign_device(gpu_id: int, logger=None) -> torch.device:
+    if torch.cuda.is_available():
+        device = torch.device(f'cuda:{gpu_id}')
+        # LOG: "Using CUDA device cuda:0"
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+        # LOG: "CUDA unavailable. Using Apple MPS device"
+        # LOG WARNING: "MPS doesn't support multi-GPU"
+    else:
+        device = torch.device('cpu')
+        # LOG WARNING: "No GPU detected. Using CPU (training will be slower)"
+    return device
