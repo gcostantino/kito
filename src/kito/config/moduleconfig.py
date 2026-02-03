8021ca4c72e0c261adeb102521894a034fb85e08
Giuseppe Costantino <giuseppe.costantino95@gmail.com>
2026-02-03 10:54:25 +0100

    Update moduleconfig.py

--- src/kito/config/moduleconfig.py
+++ src/kito/config/moduleconfig.py
@@ -94,7 +94,7 @@ class TrainingConfig:
 
     # Distributed training
     distributed_training: bool = False
-    master_gpu_id: int = 0  # Only used if not distributed
+    master_gpu_id: int = 0  # Only used if distributed
 
     # Weight initialization
     initialize_model_with_saved_weights: bool = False
